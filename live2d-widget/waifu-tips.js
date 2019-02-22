350e27ead662787bbe2fae93756aa9f186555926
Evermenot <lien_fted@163.com>
2019-02-22 14:34:01 +0800

    Site updated: 2019-02-22 14:34:00

--- live2d-widget/waifu-tips.js
+++ live2d-widget/waifu-tips.js
@@ -4,7 +4,7 @@
  */
 
 function initWidget(waifuPath, apiPath) {
-	if (screen.width <= 768="" ||="" (localstorage.getitem("waifu-display")="" &&="" new="" date().gettime()="" -="" localstorage.getitem("waifu-display")="" <="86400000))" return;="" localstorage.removeitem("waifu-display");="" sessionstorage.removeitem("waifu-text");="" $("body").append(`<div="" id="waifu">
+	if (screen.width <= 768 || (localstorage.getitem("waifu-display") && new date().gettime() - localstorage.getitem("waifu-display") <="86400000))" return; localstorage.removeitem("waifu-display"); sessionstorage.removeitem("waifu-text"); $("body").append(`<div id="waifu">
 			<div id="waifu-tips"></div>
 			<canvas id="live2d" width="300" height="300"></canvas>
 			<div id="waifu-tool">
@@ -56,7 +56,7 @@ function initWidget(waifuPath, apiPath) {
 		var SiteIndexUrl = location.port ? `${location.protocol}//${location.hostname}:${location.port}/` : `${location.protocol}//${location.hostname}/`, text; //自动获取主页
 		if (location.href == SiteIndexUrl) { //如果是主页
 			var now = new Date().getHours();
-			if (now > 23 || now <= 5)="" text="你是夜猫子呀？这么晚还不睡觉，明天起的来嘛？" ;="" else="" if="" (now=""> 5 && now <= 7)="" text="早上好！一日之计在于晨，美好的一天就要开始了。" ;="" else="" if="" (now=""> 7 && now <= 11)="" text="上午好！工作顺利嘛，不要久坐，多起来走动走动哦！" ;="" else="" if="" (now=""> 11 && now <= 14)="" text="中午了，工作了一个上午，现在是午餐时间！" ;="" else="" if="" (now=""> 14 && now <= 17)="" text="午后很容易犯困呢，今天的运动目标完成了吗？" ;="" else="" if="" (now=""> 17 && now <= 19)="" text="傍晚了！窗外夕阳的景色很美丽呢，最美不过夕阳红～" ;="" else="" if="" (now=""> 19 && now <= 21)="" text="晚上好，今天过得怎么样？" ;="" else="" if="" (now=""> 21 && now <= 23)="" text="["已经这么晚了呀，早点休息吧，晚安～"," "深夜时要爱护眼睛呀！"];="" else="" ;="" }="" if="" (document.referrer="" !="=" "")="" {="" var="" referrer="document.createElement("a");" referrer.href="document.referrer;" domain="referrer.hostname.split(".")[1];" (location.hostname="=" referrer.hostname)="" +="" document.title.split('="" -="" ')[0]="" '』<="" span="">';
+			if (now > 23 || now <= 5) text="你是夜猫子呀？这么晚还不睡觉，明天起的来嘛？" ; else if (now> 5 && now <= 7) text="早上好！一日之计在于晨，美好的一天就要开始了。" ; else if (now> 7 && now <= 11) text="上午好！工作顺利嘛，不要久坐，多起来走动走动哦！" ; else if (now> 11 && now <= 14) text="中午了，工作了一个上午，现在是午餐时间！" ; else if (now> 14 && now <= 17) text="午后很容易犯困呢，今天的运动目标完成了吗？" ; else if (now> 17 && now <= 19) text="傍晚了！窗外夕阳的景色很美丽呢，最美不过夕阳红～" ; else if (now> 19 && now <= 21) text="晚上好，今天过得怎么样？" ; else if (now> 21 && now <= 23) text="["已经这么晚了呀，早点休息吧，晚安～"," "深夜时要爱护眼睛呀！"]; else ; } if (document.referrer !="=" "") { var referrer="document.createElement("a");" referrer.href="document.referrer;" domain="referrer.hostname.split(".")[1];" (location.hostname="=" referrer.hostname) + document.title.split(' - ')[0] '』< span>';
 			else if (domain == 'baidu') text = 'Hello！来自 百度搜索 的朋友<br>你是搜索 <span style="color:#0099cc;">' + referrer.search.split('&wd=')[1].split('&')[0] + '</span> 找到的我吗？';
 			else if (domain == 'so') text = 'Hello！来自 360搜索 的朋友<br>你是搜索 <span style="color:#0099cc;">' + referrer.search.split('&q=')[1].split('&')[0] + '</span> 找到的我吗？';
 			else if (domain == 'google') text = 'Hello！来自 谷歌搜索 的朋友<br>欢迎阅读<span style="color:#0099cc;">『' + document.title.split(' - ')[0] + '』</span>';
